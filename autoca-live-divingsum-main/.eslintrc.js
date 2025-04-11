1e3ddc1fc721f04fe283ed8b50bd02d0beb9d7e4
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-04-10 20:10:15 -1100

    Update .eslintrc.js

--- autoca-live-divingsum-main/.eslintrc.js
+++ autoca-live-divingsum-main/.eslintrc.js
@@ -10,51 +10,29 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@angular-eslint/recommended',
-    'plugin:@angular-eslint/template/recommended',
     'prettier', // Add Prettier integration (make sure it's last)
   ],
   rules: {
-    // Add your custom rules or overrides here
+    // Your existing rules
     'no-console': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'warn',
-
-    // More specific rules (the ones you wanted to add earlier)
     'no-unused-vars': 'warn',
     'no-empty-function': 'warn',
     'curly': 'error',
     'quotes': ['error', 'single', { 'avoidEscape': true }],
     'semi': ['error', 'always'],
-
-    // Angular specific rules (the ones you provided previously)
-    '@angular-eslint/component-selector': [
-      'error',
-      {
-        type: 'kebab-case',
-        prefix: 'app', // Or your preferred prefix
-      },
-    ],
-    '@angular-eslint/directive-selector': [
-      'error',
-      {
-        type: 'camelCase',
-        prefix: 'app', // Or your preferred prefix
-      },
-    ],
+    '@angular-eslint/component-selector': [/* ... */],
+    '@angular-eslint/directive-selector': [/* ... */],
     '@angular-eslint/no-input-rename': 'error',
     '@angular-eslint/no-output-rename': 'error',
     '@angular-eslint/no-host-metadata-property': 'error',
-
-    // TypeScript specific rules (the ones you just provided)
-    '@typescript-eslint/no-explicit-any': 'warn', // Warn against using 'any' type
-    // '@typescript-eslint/no-unused-vars': 'warn', // You might want to enable this and disable the base 'no-unused-vars'
-    '@typescript-eslint/no-inferrable-types': 'warn', // Warn against explicitly typing values that can be inferred
-    '@typescript-eslint/no-non-null-assertion': 'warn', // Warn against using the non-null assertion operator (!)
-
-    // Prettier handles most formatting rules, so you might disable conflicting ESLint rules
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
-    'quotes': 'off', // Disable ESLint's rule as Prettier will handle it
-    'semi': 'off',   // Disable ESLint's rule as Prettier will handle it
+    'quotes': 'off',
+    'semi': 'off',
     'comma-dangle': 'off',
     'object-curly-spacing': 'off',
     'space-before-blocks': 'off',
@@ -71,6 +49,20 @@ module.exports = {
       extends: ['plugin:@angular-eslint/template/recommended'],
       rules: {
         '@angular-eslint/template/banana-in-box': 'error',
+        // Add other template-specific rules here
+      },
+    },
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser', // Explicitly set the TS parser for TS files
+      plugins: ['@typescript-eslint', '@angular-eslint/eslint-plugin'], // Include necessary plugins
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@angular-eslint/recommended',
+        'prettier',
+      ],
+      rules: {
+        // Your TypeScript-specific rules (can be the same as root if desired)
       },
     },
   ],
