237dee0bb9872b180a8a5f46f8c247a024d37e9a
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-04-10 21:46:58 -1100

    Update .eslintrc.js

--- autoca-live-divingsum-main/.eslintrc.js
+++ autoca-live-divingsum-main/.eslintrc.js
@@ -1,89 +1,30 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    '@angular-eslint/template',
-    '@angular-eslint/eslint-plugin',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@angular-eslint/recommended',
-    'plugin:@angular-eslint/template/recommended',
-    'prettier', // Add Prettier integration (make sure it's last)
-  ],
-  rules: {
-    // Add your custom rules or overrides here
-    'no-console': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-
-    // More specific rules (the ones you wanted to add earlier)
-    'no-unused-vars': 'warn',
-    'no-empty-function': 'warn',
-    'curly': 'error',
-    'quotes': ['error', 'single', { 'avoidEscape': true }],
-    'semi': ['error', 'always'],
-
-    // Angular specific rules (the ones you provided previously)
-    '@angular-eslint/component-selector': [
-      'error', // Changed 'undefined' to 'error'
-      {
-        type: 'kebab-case',
-        prefix: 'app', // Or your preferred prefix
-      },
-    ],
-    '@angular-eslint/directive-selector': [
-      'error',
-      {
-        type: 'camelCase',
-        prefix: 'app', // Or your preferred prefix
-      },
-    ],
-    '@angular-eslint/no-input-rename': 'error',
-    '@angular-eslint/no-output-rename': 'error',
-    '@angular-eslint/no-host-metadata-property': 'error',
-
-    // TypeScript specific rules (the ones you just provided)
-    '@typescript-eslint/no-explicit-any': 'warn', // Warn against using 'any' type
-    // '@typescript-eslint/no-unused-vars': 'warn', // You might want to enable this and disable the base 'no-unused-vars'
-    '@typescript-eslint/no-inferrable-types': 'warn', // Warn against explicitly typing values that can be inferred
-    '@typescript-eslint/no-non-null-assertion': 'warn', // Warn against using the non-null assertion operator (!)
-
-    // Prettier handles most formatting rules, so you might disable conflicting ESLint rules
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off',
-    'quotes': 'off', // Disable ESLint's rule as Prettier will handle it
-    'semi': 'off',   // Disable ESLint's rule as Prettier will handle it
-    'comma-dangle': 'off',
-    'object-curly-spacing': 'off',
-    'space-before-blocks': 'off',
-    'space-before-function-paren': 'off',
-    'space-infix-ops': 'off',
-    'keyword-spacing': 'off',
-    'no-trailing-spaces': 'off',
-  },
+  ignorePatterns: ['projects/**/*'],
   overrides: [
-    {
-      files: ['*.component.html', '*.html'],
-      parser: '@angular-eslint/template-parser',
-      plugins: ['@angular-eslint/template'],
-      extends: ['plugin:@angular-eslint/template/recommended'],
-      rules: {
-        '@angular-eslint/template/banana-in-box': 'error',
-      },
-    },
+    // For TypeScript files
     {
       files: ['*.ts'],
-      parser: '@typescript-eslint/parser', // Explicitly set the TS parser for TS files
-      plugins: ['@typescript-eslint', '@angular-eslint/eslint-plugin'], // Include necessary plugins
+      parserOptions: {
+        project: ['tsconfig.json'],
+        createDefaultProgram: true,
+      },
       extends: [
-        'plugin:@typescript-eslint/recommended',
         'plugin:@angular-eslint/recommended',
-        'prettier',
+        'plugin:@angular-eslint/template/process-inline-templates'
       ],
       rules: {
-        // Your TypeScript-specific rules (can be the same as root if desired)
+        // Add your TS rules here
+      },
+    },
+    // For Angular templates (HTML files)
+    {
+      files: ['*.html'],
+      parser: '@angular-eslint/template-parser',
+      plugins: ['@angular-eslint/template'],
+      rules: {
+        '@angular-eslint/template/banana-in-box': 'error',
+        // Add your template rules here
       },
     },
   ],
